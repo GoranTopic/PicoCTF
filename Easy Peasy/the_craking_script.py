a423b10e26e38a6d02ee31f0097cdbd3580408e0
Goran Topic <telegonicaxx@live.com>
2022-01-10 14:23:19 -0800

    working verions of the Esay peasy

--- Easy Peasy/the_craking_script.py
+++ Easy Peasy/the_craking_script.py
@@ -1,7 +1,14 @@
 import nclib
 PAD_LEN = 50000
-user_input = "00000000000000000000000000000000"
 
+def unhex_to_int_list(hex_input): 
+    unhexed = []
+    for i in range(0, len(hex_input), 2):
+        # get the next two chars in encrypted ui
+        hexed = hex_input[i:i+2] 
+        # get the unencrypted ui char
+        unhexed.append( int(hexed, 16) )
+    return unhexed
 
 # connect to nc mercury.picoctf.net 41934
 nc = nclib.Netcat(('mercury.picoctf.net', 41934), verbose=False)
@@ -10,16 +17,15 @@ nc = nclib.Netcat(('mercury.picoctf.net', 41934), verbose=False)
 recived_message = nc.recv_until("What data would you like to encrypt?")
 
 # get the key from the message 
-encrypted_flag = recived_message.splitlines()[2].decode('utf-8')
+hexed_flag = recived_message.splitlines()[2].decode('ascii').strip()
 # messure the length of the encrypted flag and does how much of the pad it has used
-print("ecrpted flag: {}".format(encrypted_flag))
+print("ecrpted flag: {}".format(hexed_flag))
 # get the length from the message
-encrypted_flag_len = len(encrypted_flag) / 2
+encrypted_flag_len = int(len(hexed_flag) / 2)
 print(encrypted_flag_len)
 
-filling = ""
-for i in range(0, int(PAD_LEN - encrypted_flag_len)):
-    filling += "a"
+# create filling to sen tot server
+filling = "a" * (PAD_LEN - encrypted_flag_len)
 print("filling length: {}".format(len(filling)))
 
 # send filling to move the the stop counte back to the begining
@@ -28,62 +34,62 @@ nc.send_line(filling)
 # recive message
 nc.recv_until("What data would you like to encrypt?")
 
+# creatte user input
+user_input = 'a' * encrypted_flag_len
+
 # send the user input 
 nc.send_line(user_input) 
 recived_message = nc.recv_until("What data would you like to encrypt?")
 
 # get encrypted user input 
-encrypted_user_input = recived_message.splitlines()[1].decode('utf-8')
+encrypted_user_input = str(recived_message.splitlines()[1], "ascii").strip()
 
 print("ecrypt user input: {}".format(encrypted_user_input))
-print(len(encrypted_user_input) == len(encrypted_flag))
 
 # use the encrypted ui and the user input to get the pad in the position where the flag was previously encrypted.
-#key = encrypted_user_input ^ hex_user_input
+encrypted_user_input = unhex_to_int_list(encrypted_user_input)
 
 # find out the the what the key is 
 key_arr = [] # arr to store the key integers
-ui_index = 0
-for i, j in zip(range(0, len(encrypted_user_input),2), range(0, len(user_input))):
-    # get the next two chars in encrypted ui
-    hexed = encrypted_user_input[i:i+2] 
+for i in range(0, len(user_input)):
     # get the unencrypted ui char
-    ui_char = user_input[j] 
+    ui_int = ord(user_input[i]) 
+    eui_int = encrypted_user_input[i]
     # read hexadecimal as a int
-    unhexed = int(hexed, 16) 
-    # read the char as a int
-    ui_int = ord(ui_char)
     # calculate the key with the unhexed value and the ui_int
-    key_int = unhexed ^ ui_int
-    print("     {:08b} <- ui\n\
-xor {:08b} <- ecrypted ui\n\
-    ------------\n\
-     {:08b} <- key value\n".format(ui_int, unhexed, key_int))
+    key_int = eui_int ^ ui_int
+    print("\
+            {:08b} <- ui\n\
+            {:08b} <- ecrypted ui\n\
+            ------------\n\
+            {} <- key value\n".format(ui_int, eui_int, key_int))
     # add to the key array of ints
     key_arr.append(key_int)
 # make ints into a string
-key = "".join(list(map(lambda num: str(num), key_arr)))
-print("\nthe key is: {}".format("".join(key)))
+key = "".join(list(map(lambda num: chr(num), key_arr)))
+print("\nThe key is: {}".format(key))
+
+
+# unhex the flag so that you can have it 
+encrypted_flag = unhex_to_int_list(hexed_flag)
 
 # use the found key to decrypt the flag
 flag_values = []
-for i, j in zip(range(0, encrypted_flag_len,2), range(0, len(user_input))):
-    hexed = encrypted_flag[i:i+2] # get the next two chars
-    unhexed = int(hexed, 16) # read hexadecimal as a int
-    #print("{} = {:08b} = {:d}".format(hexed, unhexed, unhexed)) 
-    key_value = key_chars[kindex]
-    kindex += 1
-    flag_value = unhexed ^ key_value
-    print("unhexed: {:08b} ^ Key: {:08b} = flag: {:08b} : {}"
-            .format(unhexed, key_value, flag_value, flag_value))
+for i in range(0, len(encrypted_flag)):
+    key_value = key_arr[i]
+    encry_flag_value = encrypted_flag[i]
+    print("key_value: {}".format(key_value))
+    
+    flag_value = encry_flag_value ^ key_value
+    print("\
+            {0:08b} {0} <- encry Flag\n\
+            {1:08b} {1} <- key\n\
+            ----------\n\
+            {2:08b} {2} <- flag\n\
+            ".format(encry_flag_value, key_value, flag_value, flag_value))
     flag_values.append(chr(flag_value))
 flag = "".join(flag_values)
-print("\nthe flag is {}".format("".join(flag)))
-
-
-#print("{:08b} : ecnypted ui\n {:08b} : ui \n------------------------\n{:08b} : key".format(encrypted_ui, str(hex_ui, key)))
-
-
+print("\nthe flag is picoCTF{%s}" % flag)
 
 
 nc.close()
